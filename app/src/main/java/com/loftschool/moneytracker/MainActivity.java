c971b327a6e1fceef23ac402904ce1eaa674abd7
aspknopixx <aspknopixx@gmail.com>
2015-12-03 21:47:09 +0300

    Add onCreateView, and icon ic_menu_white_24dp

--- app/src/main/java/com/loftschool/moneytracker/MainActivity.java
+++ app/src/main/java/com/loftschool/moneytracker/MainActivity.java
@@ -1,36 +1,33 @@
 package com.loftschool.moneytracker;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setupToolbar();
 
         Log.d(LOG_TAG,"onCreate");
+    }
+
+    public View onCreateView(String name, Context context, AttributeSet attrs){
+        Log.d(LOG_TAG,"onCreateView()");
+        return super.onCreateView(name, context, attrs);
     }
 
     protected void onStart(){
@@ -51,32 +48,20 @@ public class MainActivity extends AppCompatActivity {
     }
     protected void onStop(){
         super.onStop();
-        Log.d(LOG_TAG,"onStop");
+        Log.d(LOG_TAG, "onStop");
     }
     protected void onDestroy(){
         super.onDestroy();
         Log.d(LOG_TAG,"onDestroy");
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void setupToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu_white_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
