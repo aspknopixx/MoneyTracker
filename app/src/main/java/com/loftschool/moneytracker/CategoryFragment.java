b9c9c686d61ce20278bbb65d79716427427f2741
aspknopixx <aspknopixx@gmail.com>
2015-12-09 14:26:33 +0300

    /*Вывести нужную страницу при выборе ее в навигационном меню. При выборе “Категории” вывести список с категориями, как в дизайне приложения, при выборе “Статистика” и “Настройки” вывести текст “Здесь будет статистика” и “Здесь будут настройки”.  */

--- app/src/main/java/com/loftschool/moneytracker/CategoryFragment.java
+++ app/src/main/java/com/loftschool/moneytracker/CategoryFragment.java
@@ -14,7 +14,9 @@ import android.os.Bundle;
         import java.util.List;
 
 public class CategoryFragment extends Fragment {
-
+/*Вывести нужную страницу при выборе ее в навигационном меню. При выборе
+“Категории” вывести список с категориями, как в дизайне приложения, при выборе “Статистика” и “Настройки” вывести текст “Здесь будет статистика” и “Здесь будут настройки”.
+ */
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
